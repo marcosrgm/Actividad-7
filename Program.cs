de4e769a84c39adac22329e147284c601b98dc04
Marcos <mrodriguezgarciamiguel@gmail.com>
2023-05-10 13:06:39 +0200

    guardar cambios

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("El programa consiste en adivinar un número del 1 al 15");
+Random rnd = new Random();
+int Numero = rnd.Next(1,15);
+int Intentosrestantes = 2;
+
+Console.WriteLine("Introduzca un número del 1 al 15");
+int numerousuario = Convert.ToInt32(Console.ReadLine());
+
+while (Intentosrestantes > 0)
+{
+    if (numerousuario == Numero)
+    {
+    Console.WriteLine("Es el número correcto, has ganado");
+    }
+    else if(Numero < numerousuario)
+    {
+        Console.WriteLine("El numero que debes encontrar es menor");
+    }
+    else
+    {
+        Console.WriteLine("El numero que debes encontrar es mayor");
+    }
+    Intentosrestantes--;
+    if (Intentosrestantes >0)
+    {
+        Console.WriteLine($"Te quedan {Intentosrestantes} oportunidades para encontrar el numero");
+    }
+    else
+    {
+        Console.WriteLine("Has perdido");
+    }
+    
+}
